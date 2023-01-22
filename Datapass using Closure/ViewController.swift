61db141ab9267feda8347cd274aea110d8072ded
Kavya Prajapati <kavya8958p@gmail.com>
2023-01-22 23:22:09 +0530

    Initial Commit

---
+++ Datapass using Closure/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Datapass using Closure
+//
+//  Created by Sunil Developer on 22/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
