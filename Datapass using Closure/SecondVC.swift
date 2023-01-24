94d11f92d38cf701b0cc417d264bcf3f83718e72
Kavya Prajapati <kavya8958p@gmail.com>
2023-01-24 11:15:03 +0530

    pass data

--- Datapass using Closure/SecondVC.swift
+++ Datapass using Closure/SecondVC.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class SecondVC: UIViewController {
+    let imgFb = UIImage(named: "imgFB")
+    let imgTwitter = #imageLiteral(resourceName: "imgTwitter")
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -15,10 +17,12 @@ class SecondVC: UIViewController {
     }
     
     @IBAction func btnFB(_ sender: Any) {
-        NotificationCenter.default.post(name: .facebook, object: ["name": 123])
+       // NotificationCenter.default.post(name: .facebook, object: ["name": 123])
+        NotificationCenter.default.post(name: .facebook, object: nil, userInfo: ["name": "Facebook", "image": imgFb ?? UIImage()])
     }
     
     @IBAction func btnTwiter(_ sender: Any) {
-        NotificationCenter.default.post(name: .twitter, object: ["name": 767])
+        //NotificationCenter.default.post(name: .twitter, object: ["name": 767])
+        NotificationCenter.default.post(name: .twitter, object: nil, userInfo: ["name": "Twitter", "image": imgTwitter ])
     }
 }
--- Datapass using Closure/ViewController.swift
+++ Datapass using Closure/ViewController.swift
@@ -13,24 +13,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var lbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-       // NotificationCenter.default.addObserver(self, selector: #selector(facebook(notification:)), name: .facebook, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(facebook(notification:)), name: .facebook, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(twitter(notification:)), name: .twitter, object: nil)
        
-        NotificationCenter.default.addObserver(forName: .facebook, object: nil, queue: .main) { (dic) in
-            print(dic)
-            self.lbl.text = dic.name.rawValue
-        }
+//        NotificationCenter.default.addObserver(forName: .facebook, object: nil, queue: .main) { (dic) in
+//            print(dic)
+//            self.lbl.text = dic.userInfo?["name"] as? String
+//        }
     }
     
     @objc func facebook(notification: Notification) {
-        lbl.text = "Facebook"
-        img.image = #imageLiteral(resourceName: "download")
+        lbl.text = notification.userInfo?["name"] as? String
+        img.image = notification.userInfo?["image"] as? UIImage
         navigationController?.popViewController(animated: true)
     }
     
     @objc func twitter(notification: Notification) {
-        lbl.text = "Twitter"
-        img.image = #imageLiteral(resourceName: "download (1)")
+//        lbl.text = "Twitter"
+//        img.image = #imageLiteral(resourceName: "download (1)")
+        
+        lbl.text = notification.userInfo?["name"] as? String
+        img.image = notification.userInfo?["image"] as? UIImage
         navigationController?.popViewController(animated: true)
     }
     
@@ -38,8 +41,6 @@ class ViewController: UIViewController {
         let secondVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "SecondVC") as! SecondVC
         navigationController?.pushViewController(secondVC, animated: true)
     }
-    
-
 }
 // Extension
 extension Notification.Name {
